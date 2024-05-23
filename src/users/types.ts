ac1c16e7e1881a00fc1f5ff95fdc0c59a86b1722
aissa-dev-1 <aissabedr74@gmail.com>
2024-05-23 21:16:14 +0100

    Update users types

--- src/users/types.ts
+++ src/users/types.ts
@@ -10,6 +10,18 @@ export type ChangeUserNameParams = {
   id: string;
 };
 
+export type ChangeUserAvatarParams = {
+  id: string;
+};
+
+export type ChangeUserBioParams = {
+  id: string;
+};
+
+export type ChangeUserPasswordParams = {
+  id: string;
+};
+
 export type DeleteUserParams = {
   id: string;
 };
